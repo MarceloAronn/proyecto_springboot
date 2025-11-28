4529748e2735b70733d17b992e4e239785b9b1f2
Marcelo_Aron_Condori_Flores <152832889+MarceloAronn@users.noreply.github.com>
2025-11-28 01:45:20 -0400

    Add files via upload

---
+++ examen1erparcial/examen1erparcial/src/main/java/Examen1erParcial/sis414/Sis414Application.java
@@ -0,0 +1,13 @@
+package Examen1erParcial.sis414;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sis414Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Sis414Application.class, args);
+    }
+}
+
+
